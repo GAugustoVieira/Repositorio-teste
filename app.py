852d2c6999332351af554a4226f00887c0c93c87
GAugustoVieira <augusto.vieira@aluno.ufabc.edu.br>
2022-03-09 18:20:30 -0300

    commit 2

---
+++ app.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+import random 
+import scipy.interpolate as ip
+from scipy.interpolate import CubicSpline
+from scipy.interpolate import interp1d
+from scipy.optimize import curve_fit
+import decimal
+import HessCMD
+import time
